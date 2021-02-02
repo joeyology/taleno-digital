829b9b7273961da3e7aa8046ad45d2d2f1563762
joeyology <josemari.taleno@gmail.com>
2021-02-02 11:52:25 +0800

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -49,7 +49,7 @@ export default class Body extends React.Component {
                     <script async src="https://www.googletagmanager.com/gtag/js?id=G-R97YGGKFF4"></script>
                     <script>
                     window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
+                    function gtag(){dataLayer.push(arguments)}
                     gtag('js', new Date());
 
                     gtag('config', 'G-R97YGGKFF4');
